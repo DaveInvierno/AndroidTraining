95afcf39789c035e32e5b5c2f0898adf30204700
DaveInvierno <d.invierno@klab.com>
2013-08-26 21:54:55 +0800

    Change main activity or navigation activity to fragment

--- src/com/bignerdranch/android/training/MainActivity.java
+++ src/com/bignerdranch/android/training/MainActivity.java
@@ -1,39 +1,23 @@
 package com.bignerdranch.android.training;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
-public class MainActivity extends Activity {
-
-	private Button mGeoQuiz;
-	private Button mCriminalIntent;
+public class MainActivity extends FragmentActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		mGeoQuiz = (Button)findViewById(R.id.geo_quiz);
-		mGeoQuiz.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(MainActivity.this, QuizActivity.class);
-				startActivity(i);
-			}
-		});
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.mainContainer);
 		
-		mCriminalIntent = (Button)findViewById(R.id.criminal_intent);
-		mCriminalIntent.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(MainActivity.this, CrimeActivity.class);
-				startActivity(i);
-			}
-		});
+		if(fragment == null) {
+			fragment = new MainLeftNavFragment();
+			fm.beginTransaction().add(R.id.mainContainer, fragment).commit();
+		}
 	}
 }
---
+++ src/com/bignerdranch/android/training/MainLeftNavFragment.java
@@ -0,0 +1,48 @@
+package com.bignerdranch.android.training;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MainLeftNavFragment extends Fragment {
+	
+	private Button mGeoQuiz;
+	private Button mCriminalIntent;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_mainleftnav, parent, false);
+		
+		mGeoQuiz = (Button)v.findViewById(R.id.geo_quiz);
+		mGeoQuiz.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(getActivity(), QuizActivity.class);
+				startActivity(i);
+			}
+		});
+		
+		mCriminalIntent = (Button)v.findViewById(R.id.criminal_intent);
+		mCriminalIntent.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(getActivity(), CrimeActivity.class);
+				startActivity(i);
+			}
+		});
+		
+		return v;
+	}
+}
